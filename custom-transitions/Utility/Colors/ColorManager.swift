aa8f74627a13681081429e4d3c400fef1d7962c5
Bruno Lorenzo <bruno.lorenzop@gmail.com>
2021-04-11 21:06:13 -0300

    Add album background color

--- custom-transitions/Utility/Colors/ColorManager.swift
+++ custom-transitions/Utility/Colors/ColorManager.swift
@@ -11,6 +11,7 @@ extension UIColor {
     
     static let backgroundColorGradient1 = UIColor(hex: "#AD301Bff")
     static let backgroundColorGradient2 = UIColor(hex: "#881111ff")
+    static let albumBackgroundColor = UIColor(hex: "#780F0Bff")
     static let songBackgroundColor = UIColor(hex: "#A8210Aff")
     static let textForegroundColor = UIColor(hex: "#330700ff")
 }
